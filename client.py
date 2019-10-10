b594d33dc6cd58b26e991d4ffbbf1bac8ecdfdec
Kiril0l <Kirill_1998_12@mail.ru>
2019-10-10 21:12:04 +0300

    client

--- client.py
+++ client.py
@@ -1,6 +1,19 @@
 from twisted.internet.protocol import ClientFactory
 from twisted.internet import reactor
 from twisted.protocols.basic import LineReceiver
+import event
+from threading import Thread
+import time
+
+
+class Message():
+
+    @event.Event.origin("new_message", post = True)
+    def add_messafe(self, message):
+        for _ in range(20):
+            time.sleep(2)
+        message.append("Text")
+
 
 class ChatClient(LineReceiver):
 
@@ -8,26 +21,31 @@ class ChatClient(LineReceiver):
         self.name = name
         self.state = "OFFLINE"
         self.work = True
+        event.Event(name = "new_message", callback=self.send_message)
+
 
     def connectionMade(self):
-        while self.work:
-            if self.factory.message is not None:
-                self.sendLine(self.factory.message.encode("utf-8"))
-                self.factory.message = None
+        pass
 
     def LineReceiver(self, line):
         if self.state == "ONLINE":
             print(line.decode("utf-8"))
         elif self.state == "OFFLINE":
             print(line.decode("utf-8"))
-            self.sendLine("{}".format(message).encode("utf-8"))
+            self.sendLine("{}".format(self.name).encode("utf-8"))
             self.state = "ONLINE"
 
     def connectionLost(self, reason):
         print("Lost")
 
-    def send_message(self, message):
-        self.sendLine("{}".format(message).encode("utf-8"))
+    def send_message(self, *args, **kwargs):
+        try:
+            message = MESSAGE[0]
+        except IndexError:
+            print("Error")
+        else:
+            del MESSAGE[0]
+            self.sendLine("{}".format(message).encode("utf-8"))
 
 class ChatClientFactory(ClientFactory):
 
@@ -44,14 +62,37 @@ class ChatClientFactory(ClientFactory):
 
     def buildProtocol(self, addr):   #создает соединение
         self.connection = ChatClient(self.name)
+        message = Message()
+        worker = Thread(target=message.add_message, args=(MESSAGE,))
+        worker.start()
         return self.connection
 
     def send(self, message):
         self.connection.send_message(message)
 
+
+
+
 if __name__ == "__main__":
-    import time
-    # сделать парсер аргументов
+    # import event
+    #
+    # def executor():
+    #     print ("выполнелось событие")
+    # class Test():
+    #
+    #
+    #     @event.Event.origin("rest", post= True)
+    #     def worker(self):
+    #         print ("я генерирую союытие")
+    #
+    # ev = event.Event(name="rest")
+    # ev.register("rest", executor) #то, что должно случится, если происходит событие
+    #
+    # test = Test()
+    # test.worker()
+
+
+    # # сделать парсер аргументов
     chat = ChatClientFactory("Kirill")
     reactor.connectTCP("192.168.4.123", 5000, chat)
     reactor.run()
