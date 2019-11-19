4d470baf382c96347300e0316d95b8e29f21f8c0
Kiril0l <Kirill_1998_12@mail.ru>
2019-11-19 21:31:59 +0300

    sa

--- yyy/test.py
+++ yyy/test.py
@@ -7,19 +7,30 @@ class TestCalcModel(unittest.TestCase):
         self.test_data = {
             "arg1": "5",
             "arg2": "2",
-            "arg3": "*"
+            "action": "*"
         }
     def test_inp(self):
         self.assertEqual(
             10,
             calculato.Calc.inpData(
-                # self.test_data
-            self.test_data.get("arg1"),
-            self.test_data.get("arg2"),
-            self.test_data.get("arg3")
+                # self.test_data.get("arg1")
+                self.test_data.get("arg1"),
+                self.test_data.get("arg2"),
+                self.test_data.get("action")
             )
         )
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+# test_data = {
+#     "arg1": "5",
+#     "arg2": "2",
+#     "action": "*"
+#         }
+# print(test_data.get("arg1"),
+#     test_data.get("arg2"),
+#     test_data.get("action"))
